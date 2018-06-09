2d5f8f4d823a07cde2c26ee9ed9b3b37585542e7
Dheerasameer Kottapalli <kdheerasameer@Ks-MacBook-Pro.local>
2018-06-09 07:40:38 +0530

    Add mock data and use for api calls

--- GoJekContacts/Helpers/ContactsHelper.swift
+++ GoJekContacts/Helpers/ContactsHelper.swift
@@ -14,50 +14,54 @@ class ContactsHelper {
   
   static func fetchAllContacts(completion: @escaping ([ContactModel]?, Bool, Error?) -> Void) {
     var contactList: [ContactModel]? = nil
-    var success: Bool = false
-    let url = URL(string: contactsBaseURL)
-    let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
-      if error == nil {
-        var jsonError: NSError? = nil
-        if let responseData = data {
-          let responseJson = try? JSONSerialization.jsonObject(with: responseData, options: .mutableContainers) as! Array<NSDictionary>
-          if let contactsJsonList = responseJson {
-            success = true
-            contactList = ContactParseHelper().getParsedContactsList(contactsJsonList)
-          } else {
-            jsonError = NSError(domain: "GO-JEK", code: 0001, userInfo: [NSLocalizedDescriptionKey: "Error in JSON Serialization"])
-          }
-          completion(contactList, success, jsonError)
-        }
-      }
-      else {
-        completion(nil, false, error)
-      }
-    }
-    task.resume()
+//    var success: Bool = false
+//    let url = URL(string: contactsBaseURL)
+//    let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
+//      if error == nil {
+//        var jsonError: NSError? = nil
+//        if let responseData = data {
+//          let responseJson = try? JSONSerialization.jsonObject(with: responseData, options: .mutableContainers) as! Array<NSDictionary>
+//          if let contactsJsonList = responseJson {
+//            success = true
+//            contactList = ContactParseHelper().getParsedContactsList(contactsJsonList)
+//          } else {
+//            jsonError = NSError(domain: "GO-JEK", code: 0001, userInfo: [NSLocalizedDescriptionKey: "Error in JSON Serialization"])
+//          }
+//          completion(contactList, success, jsonError)
+//        }
+//      }
+//      else {
+//        completion(nil, false, error)
+//      }
+//    }
+//    task.resume()
+    contactList = ContactParseHelper().getParsedContactsList(MockData.mockContactsList)
+    completion(contactList, true, nil)
   }
   
   static func fetchContactDetails(_ contact: ContactModel, completion: @escaping (Bool, Error?) -> Void) {
-    var success: Bool = false
-    let url = URL(string: contact.details!.url!)
-    let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
-      if error == nil {
-        var jsonError: NSError? = nil
-        if let responseData = data {
-          let responseJson = try? JSONSerialization.jsonObject(with: responseData, options: .mutableContainers) as! NSDictionary
-          if let detailsJson = responseJson {
-            success = true
-            ContactParseHelper().parseAndUpdateContactDetails(withDetails: detailsJson, for: contact)
-          } else {
-            jsonError = NSError(domain: "GO-JEK", code: 0002, userInfo: [NSLocalizedDescriptionKey: "Error in JSON Serialization"])
-          }
-          completion(success, jsonError)
-        }
-      }
-      else {
-        completion(false, error)
-      }
-    }
-    task.resume()
+//    var success: Bool = false
+//    let url = URL(string: contact.details!.url!)
+//    let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
+//      if error == nil {
+//        var jsonError: NSError? = nil
+//        if let responseData = data {
+//          let responseJson = try? JSONSerialization.jsonObject(with: responseData, options: .mutableContainers) as! NSDictionary
+//          if let detailsJson = responseJson {
+//            success = true
+//            ContactParseHelper().parseAndUpdateContactDetails(withDetails: detailsJson, for: contact)
+//          } else {
+//            jsonError = NSError(domain: "GO-JEK", code: 0002, userInfo: [NSLocalizedDescriptionKey: "Error in JSON Serialization"])
+//          }
+//          completion(success, jsonError)
+//        }
+//      }
+//      else {
+//        completion(false, error)
+//      }
+//    }
+//    task.resume()
+    ContactParseHelper().parseAndUpdateContactDetails(withDetails: MockData.mockContactDetailsDictionary, for: contact)
+    completion(true, nil)
   }
 }
